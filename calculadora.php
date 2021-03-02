8ed460289a6541b4d27dedc67c3ae581fcf358c4
RichelleG <78814708+RichelleG@users.noreply.github.com>
2021-03-02 08:22:37 -0800

    Create calculadora.php

---
+++ calculadora.php
@@ -0,0 +1,18 @@
+<?php
+  $operando1 = $_GET['operando1'];
+  $operando2 = $_GET['operando2'];
+  $operador = $_GET['operador'];
+
+  if($operador == "+"){
+    $solucion = $operando1 + $operando2;
+  }else if ($operador == "-"){
+    $solucion = $operando1 - $operando2;
+  }else if ($operador == "*"){
+    $solucion = $operando1 * $operando2;
+  }else if ($operador == "/"){
+    $solucion = $operando1 / $operando2;
+  }
+  
+  echo "La solucion es: ".$solucion;
+  
+?>
